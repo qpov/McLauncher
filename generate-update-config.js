e8400d10ac1a8e80f560e0b0b301cf46a6e1025f
pov <aset.slanbek@gmail.com>
2025-03-14 02:00:29 +0500

    1

--- generate-update-config.js
+++ generate-update-config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const crypto = require("crypto");
 
 const rootDir = process.argv[2] || ".";
-const baseUri = process.argv[3] || "https://raw.githubusercontent.com/qpov/McLauncher/main/";
+const baseUriRemote = process.argv[3] || "https://raw.githubusercontent.com/qpov/McLauncher/main/";
 const outputFile = process.argv[4] || "update4j-config.xml";
 
 const exclude = [".git", ".gitignore", outputFile, "generate-update-config.js"];
@@ -12,7 +12,6 @@ function walkDir(dir, fileList = []) {
     const entries = fs.readdirSync(dir, { withFileTypes: true });
     for (const entry of entries) {
         if (exclude.includes(entry.name) || entry.name.startsWith(".")) continue;
-
         const fullPath = path.join(dir, entry.name);
         if (entry.isDirectory()) {
             walkDir(fullPath, fileList);
@@ -29,18 +28,18 @@ function walkDir(dir, fileList = []) {
 
 function generateConfig() {
     const fileList = walkDir(rootDir);
-    // Получаем абсолютный путь и преобразуем его в file URI (для Windows получится, например, file:///A:/Projects/McLauncher)
+    // Получаем абсолютный путь и преобразуем его в file URI (например, file:///A:/Projects/McLauncher)
     let baseDir = path.resolve(rootDir).replace(/\\/g, "/");
-    // Если путь не начинается со слэша, добавляем его (для корректного URI)
     if (!baseDir.startsWith("/")) {
        baseDir = "/" + baseDir;
     }
-    const fileUri = "file://" + baseDir;
+    const baseUriLocal = "file://" + baseDir;
     
     let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
-    xml += `<configuration base="${fileUri}">\n  <files>\n`;
+    xml += `<configuration base="${baseUriLocal}">\n  <files>\n`;
     fileList.forEach(file => {
-        xml += `    <file uri="${baseUri}${file.relPath}" path="${file.relPath}" sha1="${file.sha1}" size="${file.size}" />\n`;
+        // Для каждого файла в path используем абсолютный URI, получая его как baseUriLocal + "/" + относительный путь
+        xml += `    <file uri="${baseUriRemote}${file.relPath}" path="${baseUriLocal}/${file.relPath}" sha1="${file.sha1}" size="${file.size}" />\n`;
     });
     xml += `  </files>\n</configuration>\n`;
     fs.writeFileSync(outputFile, xml);
