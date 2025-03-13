c9e943fab40a2e27811df7689f2e3f2c612bd70d
pov <aset.slanbek@gmail.com>
2025-03-14 00:42:21 +0500

    1

--- generate-update-config.js
+++ generate-update-config.js
@@ -1,18 +1,13 @@
 // node generate-update-config.js . "https://raw.githubusercontent.com/qpov/McLauncher/main/" update4j-config.xml
-
 const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 
-// Корневая папка для сканирования (обычно корень проекта)
 const rootDir = process.argv[2] || ".";
-// Базовый URI для файлов (например, URL на GitHub)
 const baseUri = process.argv[3] || "https://raw.githubusercontent.com/qpov/McLauncher/main/";
-// Имя выходного XML-файла конфигурации
 const outputFile = process.argv[4] || "update4j-config.xml";
 
-// Файлы/папки, которые не нужно включать
-const exclude = [".git", ".gitignore", "update4j-config.xml"];
+const exclude = [".git", ".gitignore", "update4j-config.xml", "generate-update-config.js"];
 
 function walkDir(dir, fileList = []) {
     const entries = fs.readdirSync(dir, { withFileTypes: true });
@@ -22,13 +17,13 @@ function walkDir(dir, fileList = []) {
         if (entry.isDirectory()) {
             walkDir(fullPath, fileList);
         } else {
-            const relPath = path.relative(rootDir, fullPath).split(path.sep).join("/");
+            const relPath = path.relative(rootDir, fullPath).replace(/\\/g, "/");
             const fileBuffer = fs.readFileSync(fullPath);
             const hashSum = crypto.createHash("sha1");
             hashSum.update(fileBuffer);
-            const sha1 = hashSum.digest("hex");
+            const hex = hashSum.digest("hex");
             const stats = fs.statSync(fullPath);
-            fileList.push({ relPath, sha1, size: stats.size });
+            fileList.push({ relPath, sha1: hex, size: stats.size });
         }
     }
     return fileList;
@@ -40,7 +35,8 @@ function generateConfig() {
     let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
     xml += `<configuration base="${baseDir}">\n  <files>\n`;
     fileList.forEach(file => {
-        xml += `    <file uri="${baseUri}${file.relPath}" path="${file.relPath}" sha1="${file.sha1}" size="${file.size}" />\n`;
+        const absoluteFilePath = path.join(baseDir, file.relPath).replace(/\\/g, "/");
+        xml += `    <file uri="${baseUri}${file.relPath}" path="${absoluteFilePath}" sha1="${file.sha1}" size="${file.size}" />\n`;
     });
     xml += `  </files>\n</configuration>\n`;
     fs.writeFileSync(outputFile, xml);
