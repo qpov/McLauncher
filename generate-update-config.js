49c38a1557503a9d2153b58e52996e7d742c6f28
pov <aset.slanbek@gmail.com>
2025-03-14 01:00:27 +0500

    1

--- generate-update-config.js
+++ generate-update-config.js
@@ -7,24 +7,23 @@ const rootDir = process.argv[2] || ".";
 const baseUri = process.argv[3] || "https://raw.githubusercontent.com/qpov/McLauncher/main/";
 const outputFile = process.argv[4] || "update4j-config.xml";
 
-const exclude = new Set([".git", ".gitignore", "update4j-config.xml", "generate-update-config.js"]);
+const exclude = [".git", ".gitignore", "update4j-config.xml", "generate-update-config.js"];
 
 function walkDir(dir, fileList = []) {
     const entries = fs.readdirSync(dir, { withFileTypes: true });
     for (const entry of entries) {
-        if (exclude.has(entry.name) || entry.name.startsWith(".")) continue;
+        if (exclude.includes(entry.name) || entry.name.startsWith(".")) continue;
         const fullPath = path.join(dir, entry.name);
         if (entry.isDirectory()) {
             walkDir(fullPath, fileList);
         } else {
-            const absolutePath = path.resolve(fullPath).replace(/\\/g, '/');
-            const relPath = path.relative(rootDir, fullPath).replace(/\\/g, '/');
+            const relPath = path.relative(rootDir, fullPath).split(path.sep).join("/");
             const fileBuffer = fs.readFileSync(fullPath);
             const hashSum = crypto.createHash("sha1");
             hashSum.update(fileBuffer);
             const hex = hashSum.digest("hex");
             const stats = fs.statSync(fullPath);
-            fileList.push({ absolutePath, relPath, sha1: hex, size: stats.size });
+            fileList.push({ relPath, sha1: hex, size: stats.size });
         }
     }
     return fileList;
@@ -32,15 +31,16 @@ function walkDir(dir, fileList = []) {
 
 function generateConfig() {
     const fileList = walkDir(rootDir);
-    const baseDir = path.resolve(rootDir).replace(/\\/g, '/');
+    const baseDir = path.resolve(rootDir);
     let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
     xml += `<configuration base="${baseDir}">\n  <files>\n`;
     fileList.forEach(file => {
-        xml += `    <file uri="${baseUri}${file.relPath}" path="${file.absolutePath}" sha1="${file.sha1}" size="${file.size}" />\n`;
+        const absolutePath = path.resolve(rootDir, file.relPath);
+        xml += `    <file uri="${baseUri}${file.relPath.replace(/\\/g, "/")}" path="${absolutePath}" sha1="${file.sha1}" size="${file.size}" />\n`;
     });
     xml += `  </files>\n</configuration>\n`;
     fs.writeFileSync(outputFile, xml);
-    console.log(`Файл ${outputFile} успешно создан.`);
+    console.log(`Файл ${outputFile} сгенерирован успешно.`);
 }
 
 generateConfig();
